0004f931d96a42c528c04ddd32e7cb27905be9b4
jlm228 <jlm228@student.bham.ac.uk>
2026-08-17 11:46:27 +0100

    Create eval YAML atomically to avoid races in evaluation

--- utils/mlflow.py
+++ utils/mlflow.py
@@ -15,8 +15,20 @@ def log_config(path_results, runid, config):
         if file.endswith(".yml"):
             tmp = int(file.split(".")[0].split("_")[-1])
             eval_id = tmp + 1 if tmp + 1 > eval_id else eval_id
-    yaml_filename = path_results + "eval_" + str(eval_id) + ".yml"
-    with open(yaml_filename, "w") as outfile:
+
+    # Claim the id by creating the file exclusively, advancing on collision. Evaluations of one
+    # run are launched concurrently (the cropped and full-resolution configs share a run id), and
+    # picking the id from a directory listing alone lets both processes choose the same one and
+    # interleave their writes into a single unparseable file.
+    while True:
+        yaml_filename = path_results + "eval_" + str(eval_id) + ".yml"
+        try:
+            fd = os.open(yaml_filename, os.O_CREAT | os.O_EXCL | os.O_WRONLY)
+            break
+        except FileExistsError:
+            eval_id += 1
+
+    with os.fdopen(fd, "w") as outfile:
         yaml.dump(config, outfile, default_flow_style=False)
 
     mlflow.start_run(runid)
